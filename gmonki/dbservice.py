b9414c86d9a1f7bd67403637883b09861e887a78
Markosys iMac <bitbucket@marktward.name>
2014-03-26 22:20:40 -0700

    shaky but working synch between flask-user member profile and neo4j graph db

--- gmonki/dbservice.py
+++ gmonki/dbservice.py
@@ -9,13 +9,16 @@ from py2neo import neo4j
 from py2neo.packages.urimagic import URI
 
 def get_graph_db():
-	app.logger.info('get_graph_db(): attempt to establish neo4j connection')
 	if not hasattr(app, 'gdb_client'):
+		app.logger.info('get_graph_db(): try to ESTABLISH neo4j connection')
 		try:
 			gdb_url = app.config['GRAPHENEDB_URL']			
 			service_root = neo4j.ServiceRoot(URI(gdb_url).resolve('/'))
+			app.gdb_client = service_root.graph_db
 			app.logger.info('neo4j: ' + str(service_root.graph_db))
-			return service_root.graph_db
+			return app.gdb_client
 		except:
 			app.logger.error('neo4j: problems acquring graph db')
+	else:
+		app.logger.info('get_graph_db(): neo4j connection EXISTS')
 
