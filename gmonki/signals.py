4daf86af50ece726241b2b891f14869affdb7dd8
Markosys MBPro <bitbucket@marktward.com>
2014-03-28 18:25:38 -0700

    migrated to cypher MERGE statement for entry.  MUCH better than previous get_or_create using external index.

--- gmonki/signals.py
+++ gmonki/signals.py
@@ -2,28 +2,31 @@
 from gmonki import app
 from flask.ext.user.signals import user_confirmed_email, user_changed_username
 from gmonki import dbservice
-from py2neo import node
+from py2neo import neo4j
 
 @user_confirmed_email.connect_via(app)
 def user_confirmed_email_action(sender, user, **extra):
-	sender.logger.info('SIGNAL user_confirmed_email: ' + str(user.id) + ' ' + str(user.username))
 	
+	# neo4j cypher query to create or merge into existing node based on fuid and username
+	cypher_query_template = 'merge (p:person {username:"%s", fuid:%d}) return p'
+
 	# create new unique graph db node with flask-user id and username
 	try:
 		dbservice.get_graph_db()
-		gdb_user = app.gdb_client.get_or_create_indexed_node('fuid', 'fuid', user.id, {'fuid':user.id, 'username':user.username})
-		gdb_user.add_labels('entity', 'person')
+		new_node_query = neo4j.CypherQuery(app.gdb_client, (cypher_query_template % (user.username, user.id)))
+		new_node_result = new_node_query.execute() 
+		sender.logger.info('user_confirmed_email: ' + str(new_node_result[0]))
 	except:
 		sender.logger.error('create_gmonki_graph_user: ERROR get_graph_db()')
 
 @user_changed_username.connect_via(app)
 def user_changed_name_action(sender, user, **extra):
-	sender.logger.info('SIGNAL user_changed_name: ' + str(user.id) + ' ' + str(user.username))
 	
 	# synch username between flask-user and  graph db
 	try:
 		dbservice.get_graph_db()
 		gdb_user, = app.gdb_client.find('person',property_key='fuid', property_value=user.id)
 		gdb_user['username'] = user.username
+		sender.logger.info('SIGNAL user_changed_name: ' + str(gdb_user))
 	except:
 		sender.logger.error('create_gmonki_graph_user: ERROR get_graph_db()')
