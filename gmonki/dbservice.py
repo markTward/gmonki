c64f802448cbfd37d10b3396ea301199a8c0d2c6
Markosys MBPro <bitbucket@marktward.com>
2014-03-28 17:34:32 -0700

    cleaner method for acquiring neo4j database

--- gmonki/dbservice.py
+++ gmonki/dbservice.py
@@ -6,19 +6,16 @@
 import os
 from gmonki import app
 from py2neo import neo4j
-from py2neo.packages.urimagic import URI
 
 def get_graph_db():
 	if not hasattr(app, 'gdb_client'):
-		app.logger.info('get_graph_db(): try to ESTABLISH neo4j connection')
 		try:
-			gdb_url = app.config['GRAPHENEDB_URL']			
-			service_root = neo4j.ServiceRoot(URI(gdb_url).resolve('/'))
-			app.gdb_client = service_root.graph_db
-			app.logger.info('neo4j: ' + str(service_root.graph_db))
+			gdb_url = app.config['GRAPHENEDB_URL'] + '/db/data/'
+			app.gdb_client = neo4j.GraphDatabaseService(gdb_url)
+			app.logger.info('py2neo: SUCCESS ' + str(app.gdb_client.graph_db))
 			return app.gdb_client
 		except:
-			app.logger.error('neo4j: problems acquring graph db')
+			app.logger.error('py2neo: ERROR problems acquring graph db')
 	else:
 		app.logger.info('get_graph_db(): neo4j connection EXISTS')
 
