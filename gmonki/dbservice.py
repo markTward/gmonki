db5c4743d2743c0a9200d72378f4eab04005d95b
Markosys iMac <bitbucket@marktward.name>
2014-04-06 23:53:18 -0700

    relocated neo4j upsert function now that it is minimally debugged

--- gmonki/dbservice.py
+++ gmonki/dbservice.py
@@ -7,6 +7,7 @@ import os
 from gmonki import app
 from py2neo import neo4j
 
+# establisth neo4j client and associated with the app
 def get_graph_db():
 	if not hasattr(app, 'gdb_client'):
 		try:
@@ -18,4 +19,23 @@ def get_graph_db():
 			app.logger.error('py2neo: ERROR problems acquring graph db')
 	else:
 		app.logger.info('get_graph_db(): neo4j connection EXISTS')
+
+# common function to create/merge neo4j node corresponding to user id
+def gdb_user_upsert(user):
+    # create or merge cypher query template
+    upsert_query_string = ('merge (p:person {{id:{uid}}}) '
+                                'on create set p.username="{uname}", p.created=timestamp() '
+                                'on match  set p.username="{uname}", p.accessed=timestamp() '
+                                'return p').format(uid=user.id, uname=user.username)
+    # attempt to create user node
+    try:
+        get_graph_db()
+        upsert_query = neo4j.CypherQuery(app.gdb_client, upsert_query_string)
+        app.logger.info('cypher qs: ' + str(upsert_query.string))
+        upsert_result = upsert_query.execute()
+        app.logger.info('gdb_user_upsert SUCCESS: ' + str(upsert_result[0]))
+        return upsert_result
+    except Exception, e:
+        app.logger.error('gdb_user_upsert failed for User ID: ' + str(user.id) + str(e.__class__))
+        return None
 
--- gmonki/signal_sub.py
+++ gmonki/signal_sub.py
@@ -4,30 +4,11 @@ from flask.ext.user.signals import user_confirmed_email, user_changed_username, 
 from gmonki import dbservice
 from py2neo import neo4j
 
-# common function to create/merge neo4j node corresponding to user id
-def gdb_user_upsert(user):
-	# create or merge cypher query template
-	upsert_query_string = ('merge (p:person {{id:{uid}}}) '
-								'on create set p.username="{uname}", p.created=timestamp() '
-								'on match  set p.username="{uname}", p.accessed=timestamp() '
-								'return p').format(uid=user.id, uname=user.username)
-	# attempt to create user node
-	try:
-		dbservice.get_graph_db()
-		upsert_query = neo4j.CypherQuery(app.gdb_client, upsert_query_string)
-		app.logger.info('cypher qs: ' + str(upsert_query.string))
-		upsert_result = upsert_query.execute()
-		app.logger.info('gdb_user_upsert SUCCESS: ' + str(upsert_result[0]))
-		return upsert_result
-	except Exception, e:
-		app.logger.error('gdb_user_upsert failed for User ID: ' + str(user.id) + str(e.__class__))
-		return None
-
 @user_confirmed_email.connect_via(app)
 def user_confirmed_email_action(sender, user, **extra):
 	# create new unique graph db node matching flask-user id
 	try:
-		upsert_result = gdb_user_upsert(user)	
+		upsert_result = dbservice.gdb_user_upsert(user)	
 		app.logger.info('user_confirmed_email_action SUCCESS User ID :: Name :: Node ==>' + 
 							str(user.id) + " :: " + str(user.username)  + " :: " + str(upsert_result[0][0]._id))
 	except:
@@ -37,7 +18,7 @@ def user_confirmed_email_action(sender, user, **extra):
 def user_changed_name_action(sender, user, **extra):
 	# create new unique graph db node matching flask-user id
 	try:
-		upsert_result = gdb_user_upsert(user)	
+		upsert_result = dbservice.gdb_user_upsert(user)	
 		app.logger.info('user_changed_name_action SUCCESS User ID :: Name :: Node ==>' + 
 							str(user.id) + " :: " + str(user.username)  + " :: " + str(upsert_result[0][0]._id))
 	except:
@@ -47,7 +28,7 @@ def user_changed_name_action(sender, user, **extra):
 def user_logged_in_action(sender, user, **extra):
 	# create new unique graph db node matching flask-user id
 	try:
-		upsert_result = gdb_user_upsert(user)	
+		upsert_result = dbservice.gdb_user_upsert(user)	
 		app.logger.info('user_logged_in_action SUCCESS User ID :: Name :: Node ==>' + 
 							str(user.id) + " :: " + str(user.username)  + " :: " + str(upsert_result[0][0]._id))
 	except:
